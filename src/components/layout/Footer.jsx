5c0f1c9a0dac37a5e6867c0fd5f7f72a80b1f9cb
Artem Danko <danko5012@gmail.com>
2025-05-10 21:10:43 +0300

    update for my variant

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,8 +1,10 @@
 function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="bg-dark text-white py-2 mt-auto">
       <div className="container-fluid px-4 text-center">
-        <p className="mb-0">&copy; 2025 Сайт про зайців</p>
+        <p className="mb-0">&copy; {currentYear} Сайт про гепардів</p>
       </div>
     </footer>
   );
