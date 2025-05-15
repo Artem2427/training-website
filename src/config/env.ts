e57dd7e66a490250f17606b45b1ddc6014d14456
Artem Danko <danko5012@gmail.com>
2025-05-16 01:46:38 +0300

    fix: update port

--- src/config/env.ts
+++ src/config/env.ts
@@ -17,5 +17,5 @@ const getEnvValue = (key: string, defaultValue: string): string => {
 
 // Експортуємо змінні середовища з типізацією та значеннями за замовчуванням
 export const NODE_ENV: string = getEnvValue('NODE_ENV', 'development');
-export const PORT: number = parseInt(getEnvValue('PORT', '5000'), 10);
+export const PORT: number = parseInt(getEnvValue('PORT', '5001'), 10);
 export const MONGODB_URI: string = getEnvValue('MONGODB_URI', 'mongodb://localhost:27017/cheetahs');
